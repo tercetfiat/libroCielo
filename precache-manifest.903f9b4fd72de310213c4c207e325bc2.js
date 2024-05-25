90b8036a503dc995e7a1a7fd28407714e2bbdcf2
BrahianVT <pumasemj@hotmail.com>
2024-05-25 09:59:09 -0600

    Updates

---
+++ precache-manifest.903f9b4fd72de310213c4c207e325bc2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd7fac6d18fa9461178",
+    "url": "/libroCielo/static/js/0.f6234a8b.chunk.js"
+  },
+  {
+    "revision": "e0afb7b787297d7f320a",
+    "url": "/libroCielo/static/js/1.1aea22bb.chunk.js"
+  },
+  {
+    "revision": "1fce4f815acc50a681c3",
+    "url": "/libroCielo/static/js/main.64660e36.chunk.js"
+  },
+  {
+    "revision": "25c8e7ef0586be47845d",
+    "url": "/libroCielo/static/js/runtime~main.1088c7d2.js"
+  },
+  {
+    "revision": "a1579e42a81baa2bd7f9",
+    "url": "/libroCielo/static/js/4.8dc859ce.chunk.js"
+  },
+  {
+    "revision": "401df6efb82209fd7d46",
+    "url": "/libroCielo/static/js/5.c788251d.chunk.js"
+  },
+  {
+    "revision": "f8e6ae368e15547af114",
+    "url": "/libroCielo/static/css/6.767572f1.chunk.css"
+  },
+  {
+    "revision": "f8e6ae368e15547af114",
+    "url": "/libroCielo/static/js/6.70d04bc2.chunk.js"
+  },
+  {
+    "revision": "34016ba3ecc33772fede",
+    "url": "/libroCielo/static/js/7.da7cf868.chunk.js"
+  },
+  {
+    "revision": "9b8f2e696041d666a6fc",
+    "url": "/libroCielo/static/css/8.f2bf5c73.chunk.css"
+  },
+  {
+    "revision": "9b8f2e696041d666a6fc",
+    "url": "/libroCielo/static/js/8.14f31c7f.chunk.js"
+  },
+  {
+    "revision": "5c87dd23bd537fd80769",
+    "url": "/libroCielo/static/js/9.a877503f.chunk.js"
+  },
+  {
+    "revision": "44f631d8be64b31021ca",
+    "url": "/libroCielo/static/js/10.4ea67124.chunk.js"
+  },
+  {
+    "revision": "e981e5af51706528a788",
+    "url": "/libroCielo/static/js/11.9021d4d2.chunk.js"
+  },
+  {
+    "revision": "097ff95c7ec3e825c8a3",
+    "url": "/libroCielo/static/js/12.a9c0451d.chunk.js"
+  },
+  {
+    "revision": "0b67bf0a844256f3853c",
+    "url": "/libroCielo/static/js/13.7c4e9976.chunk.js"
+  },
+  {
+    "revision": "d82916c49eecae1bfaae",
+    "url": "/libroCielo/static/js/14.fc2d8473.chunk.js"
+  },
+  {
+    "revision": "3cda4087b72dcef9eb2b",
+    "url": "/libroCielo/static/js/15.91d9daa0.chunk.js"
+  },
+  {
+    "revision": "d9f7f2cd279b4bacfb09",
+    "url": "/libroCielo/static/js/16.04a42eae.chunk.js"
+  },
+  {
+    "revision": "f493df287adde6dfaf75",
+    "url": "/libroCielo/static/js/17.585097dc.chunk.js"
+  },
+  {
+    "revision": "0818290c3de9868a2212",
+    "url": "/libroCielo/static/js/18.0cb8c1f3.chunk.js"
+  },
+  {
+    "revision": "ded5d505dd4eec267fbc",
+    "url": "/libroCielo/static/js/19.b25bad03.chunk.js"
+  },
+  {
+    "revision": "7e5935d65f2aead35a822f0bfe4e5ff9",
+    "url": "/libroCielo/index.html"
+  }
+];
